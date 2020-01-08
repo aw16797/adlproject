6fa8b881d4bafd7e9447018d085f993af6f50d10
Angus Williams <aw16797@my.bristol.ac.uk>
2020-01-08 12:53:28 +0000

    Update & rename main script
    Rewrote to resemble lab4.py, using trainer, not using for-loops in main. Added correct parsers. Updated CNN model.

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
-import torch
 import time
 import argparse
 from pathlib import Path
+
 from multiprocessing import cpu_count
+from typing import Union, NamedTuple
 
 import torch
 import torchvision
+import torchvision.datasets
 import torch.backends.cudnn
 import numpy as np
-import torchvision.transforms as trfm
+from torchvision.transforms import ToTensor
 from torch import nn, optim
 from torch.nn import functional as F
 from torch.optim.optimizer import Optimizer
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-
 from dataset import UrbanSound8KDataset
 
 torch.backends.cudnn.benchmark = True
+
+parser = argparse.ArgumentParser(
+    description="Train CNN on UrbanSound8K dataset",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument("--log-dir", default=Path("logs"), type=Path)
+parser.add_argument("--mode", default="LMC", type=string, help="Which feature mode to execute for (MC, LMC or MLMC)")
+parser.add_argument("--learning-rate", default=1e-3, type=float, help="Learning rate")
+parser.add_argument("--dropout", default=0.5, type=float, help="Dropout variable")
+parser.add_argument("--batch-size", default=32, type=int, help="Number of samples within each mini-batch")
+parser.add_argument("--epochs", default=20, type=int, help="Number of epochs to train for")
+parser.add_argument("--workers", default=8, type=int, help="Number of workers for loaders")
+parser.add_argument("--decay", default=1e-3, type=float, help="Weight decay to use in SGD Optimizer")
+parser.add_argument("--momentum", default=0.9, type=float, help="Learning rate")
+parser.add_argument("--val-frequency", default=2, type=int, help="How frequently to test the model on the validation set in number of epochs")
+parser.add_argument("--log-frequency", default=10, type=int, help="How frequently to save logs to tensorboard in number of steps")
+parser.add_argument("--print-frequency", default=10, type=int, help="How frequently to print progress to the command line in number of steps")
 
 if torch.cuda.is_available():
     DEVICE = torch.device("cuda")
@@ -26,32 +44,29 @@ else:
 
 
 def main(args):
-    tensor = trfm.ToTensor()
-    transforms = [tensor]
-
-    args.dataset_root.mkdir(parents=True, exist_ok=True)
-
-    composed = Compose(transforms)
+    transform = ToTensor()
 
     train_loader = torch.utils.data.DataLoader( 
-        UrbanSound8KDataset(‘UrbanSound8K_train.pkl’, mode), 
-        batch_size=32,
+        UrbanSound8KDataset(‘UrbanSound8K_train.pkl’, args.mode), 
+        batch_size=args.batch_size,
         shuffle=True, 
-        num_workers=8,
+        num_workers=arg.workers,
         pin_memory=True,
     ) 
 
      val_loader = torch.utils.data.DataLoader( 
-        UrbanSound8KDataset(‘UrbanSound8K_test.pkl’, mode), 
-        batch_size=32,
+        UrbanSound8KDataset(‘UrbanSound8K_test.pkl’, args.mode), 
+        batch_size=args.batch_size,
         shuffle=False, 
-        num_workers=8,
+        num_workers=args.workers,
         pin_memory=True,
     )
 
+    model = CNN(height=85, width=41, channels=1, class_count=10, dropout=args.dropout)
+
     criterion = nn.CrossEntropyLoss()
 
-    optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
+    optimizer = optim.SGD(model.parameters(), lr=args.learning_rate, momentum=args.momentum, weight_decay=args.decay)
 
     log_dir = get_summary_writer_log_dir(args)
     print(f"Writing logs to {log_dir}")
@@ -60,24 +75,19 @@ def main(args):
             flush_secs=5
     )
 
-
-    for i, (input, target, filename) in enumerate(train_loader):
-        model = CNN(height=85, width=41, channels=1, class_count=10, dropout=0.5)
-
-
-
-    for i, (input, target, filename) in enumerate(val_loader):
-        model = CNN(height=85, width=41, channels=1, class_count=10, dropout=0)
-
-
     trainer = Trainer(
         model, train_loader,  val_loader, criterion, optimizer, summary_writer, DEVICE
     )
 
-    trainer.train(epochs=1, 2, 10, 10)
+    trainer.train(
+        args.epochs,
+        args.val_frequency,
+        print_frequency=args.print_frequency,
+        log_frequency=args.log_frequency,
+    )
 
     summary_writer.close()
-}
+
 
 class CNN(nn.Module):
     def __init__(self, height: int, width: int, channels: int, class_count: int, dropout: float):
@@ -86,8 +96,10 @@ class CNN(nn.Module):
         self.class_count = class_count
 
         self.dropout = nn.Dropout(dropout)
+
         self.bn32 = nn.BatchNorm2d(32)
         self.bn64 = nn.BatchNorm2d(64)
+        self.bnFC = nn.BatchNorm1d(1024)
 
         self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
 
@@ -102,7 +114,7 @@ class CNN(nn.Module):
 
         self.conv2 = nn.Conv2d(
             in_channels=32,
-            out_channels=32,
+            out_channels=32, # should be 64?
             kernel_size=(3, 3),
             padding=(2, 2),
             stride=(2, 2),
@@ -131,7 +143,7 @@ class CNN(nn.Module):
         self.initialise_layer(self.fc1)
 
         self.fc2 = nn.Linear(1024, 10)
-        self.initialise_layer(self.fc1)
+        self.initialise_layer(self.fc2)
 
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
@@ -147,9 +159,17 @@ class CNN(nn.Module):
 
         x = torch.flatten(x,1)
 
-        x = F.sigmoid(self.fc1(self.dropout(x)))
+        # states dropout should in "2nd, 4th and Fully Connected layer"
+        # should be in both FC layers or just first?
+        # from lab 4:
+            # "Dropout is typically applied on the input to FC layers,
+            #  but can also be applied to the input of any hidden layer"
+            # "In the forward method, apply self.dropout to the input of
+            #  each fully connected layer."
 
-        x = F.softmax(self.fc2(x))
+        x = F.sigmoid(self.bnFC(self.fc1(self.dropout(x))))
+
+        x = F.softmax(self.fc2(self.dropout(x)))
 
         return x
 
@@ -160,76 +180,6 @@ class CNN(nn.Module):
         if hasattr(layer, "weight"):
             nn.init.kaiming_normal_(layer.weight)
 
-def compute_accuracy(
-    labels: Union[torch.Tensor, np.ndarray], preds: Union[torch.Tensor, np.ndarray]
-) -> float:
-    """
-    Args:
-        labels: ``(batch_size, class_count)`` tensor or array containing example labels
-        preds: ``(batch_size, class_count)`` tensor or array containing model prediction
-    """
-    assert len(labels) == len(preds)
-    return float((labels == preds).sum()) / len(labels)
-
-def compute_pca(
-    labels: Union[torch.Tensor, np.ndarray], preds: Union[torch.Tensor, np.ndarray]
-):
-    """
-    Args:
-        labels: ``(batch_size, class_count)`` tensor or array containing example labels
-        preds: ``(batch_size, class_count)`` tensor or array containing model prediction
-    """
-    assert len(labels) == len(preds)
-
-    # stores total number of examples for each class
-    class_dict = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
-
-    # stores total number of correct predictions for each class
-    correct_dict = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
-
-    # stores accuracy for each class
-    pca_dict = {0:0.0, 1:0.0, 2:0.0, 3:0.0, 4:0.0, 5:0.0, 6:0.0, 7:0.0, 8:0.0, 9:0.0}
-
-    for i in range(0,len(labels)-1):
-        class_dict[labels[i]] += 1
-        if labels[i] == preds[i]:
-            correct_dict[labels[i]] += 1
-
-    for key, val in pca_dict.items():
-        pca_dict[key] = (correct_dict[key]/class_dict[key])
-
-    return pca_dict
-
-def get_summary_writer_log_dir(args: argparse.Namespace) -> str:
-    """Get a unique directory that hasn't been logged to before for use with a TB
-    SummaryWriter.
-
-    Args:
-        args: CLI Arguments
-
-    Returns:
-        Subdirectory of log_dir with unique subdirectory name to prevent multiple runs
-        from getting logged to the same TB log directory (which you can't easily
-        untangle in TB).
-    """
-    tb_log_dir_prefix = (
-      f"CNN_bn_"
-      f"dropout={args.dropout}_"
-      f"bs={args.batch_size}_"
-      f"lr={args.learning_rate}_"
-      f"momentum=0.9_"
-      f"perspective={args.data_aug_perspective}_" +
-      f"brightness={args.data_aug_brightness}_" +
-      ("hflip_" if args.data_aug_hflip else "") +
-      f"run_"
-    )
-    i = 0
-    while i < 1000:
-        tb_log_dir = args.log_dir / (tb_log_dir_prefix + str(i))
-        if not tb_log_dir.exists():
-            return str(tb_log_dir)
-        i += 1
-    return str(tb_log_dir)
 
 class Trainer:
     def __init__(
@@ -265,6 +215,7 @@ class Trainer:
             data_load_start_time = time.time()
 
             for i, (input, target, filename) in enumerate(self.train_loader):
+
                 batch = input.to(self.device)
                 labels = target.to(self.device)
                 fles = filename.to(self.device
@@ -382,6 +333,73 @@ class Trainer:
         print(f"class 10 accuracy: {pca[9] * 100:2.2f}")
 
 
+def compute_accuracy(
+    labels: Union[torch.Tensor, np.ndarray], preds: Union[torch.Tensor, np.ndarray]
+) -> float:
+    """
+    Args:
+        labels: ``(batch_size, class_count)`` tensor or array containing example labels
+        preds: ``(batch_size, class_count)`` tensor or array containing model prediction
+    """
+    assert len(labels) == len(preds)
+    return float((labels == preds).sum()) / len(labels)
+
+def compute_pca(
+    labels: Union[torch.Tensor, np.ndarray], preds: Union[torch.Tensor, np.ndarray]
+):
+    """
+    Args:
+        labels: ``(batch_size, class_count)`` tensor or array containing example labels
+        preds: ``(batch_size, class_count)`` tensor or array containing model prediction
+    """
+    assert len(labels) == len(preds)
+
+    # stores total number of examples for each class
+    class_dict = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+
+    # stores total number of correct predictions for each class
+    correct_dict = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+
+    # stores accuracy for each class
+    pca_dict = {0:0.0, 1:0.0, 2:0.0, 3:0.0, 4:0.0, 5:0.0, 6:0.0, 7:0.0, 8:0.0, 9:0.0}
+
+    for i in range(0,len(labels)-1):
+        class_dict[labels[i]] += 1
+        if labels[i] == preds[i]:
+            correct_dict[labels[i]] += 1
+
+    for key, val in pca_dict.items():
+        pca_dict[key] = (correct_dict[key]/class_dict[key])
+
+    return pca_dict
+
+def get_summary_writer_log_dir(args: argparse.Namespace) -> str:
+    """Get a unique directory that hasn't been logged to before for use with a TB
+    SummaryWriter.
+
+    Args:
+        args: CLI Arguments
+
+    Returns:
+        Subdirectory of log_dir with unique subdirectory name to prevent multiple runs
+        from getting logged to the same TB log directory (which you can't easily
+        untangle in TB).
+    """
+    tb_log_dir_prefix = (
+      f"CNN_bn_"
+      f"dropout={args.dropout}_"
+      f"bs={args.batch_size}_"
+      f"lr={args.learning_rate}_"
+      f"momentum=0.9_"
+      f"run_"
+    )
+    i = 0
+    while i < 1000:
+        tb_log_dir = args.log_dir / (tb_log_dir_prefix + str(i))
+        if not tb_log_dir.exists():
+            return str(tb_log_dir)
+        i += 1
+    return str(tb_log_dir)
 
 if __name__ == "__main__":
     main(parser.parse_args())
